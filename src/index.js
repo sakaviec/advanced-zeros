635ae52a34b3e6c387da394530f6e6916b7663db
sakaviec <sakaviec@users.noreply.github.com>
2019-03-24 21:42:02 +0300

    feat: implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  function getPrimes(base) {
+        let b = base;
+        let obj = {};
+        for (let i = 2; i <= b; i++) {
+            while (b % i == 0) {
+                if (obj[i]) obj[i]++;
+                else obj[i] = 1;
+                b /= i;
+            }
+        }
+        return obj;
+    }
+
+    let primes = getPrimes(base);
+    let result = [];
+
+     console.log(primes)
+
+    for (let key in primes) {
+        let count = 0;
+        let n = number;
+        while (n >= 1) {
+            let times = Math.floor(n / key);
+            //console.log(n+" div. by "+key+" equals "+times);
+            n = times;
+            count += times;
+            //console.log("Count: "+count);
+        }
+        result.push(Math.floor(count / primes[key]));
+        //console.log(result);
+    }
+
+    return Math.min.apply(null, result);
+};
